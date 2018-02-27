417f1d11cd29578e3d948b3d902bfa095d4de8ad
YuChiHoon <penych1015@gmail.com>
2018-02-27 21:54:54 +0900

    2018_02_27

---
+++ JavaScript/JavaScript/Script/02/02_02_롤오버기능적용하기2.js
@@ -0,0 +1,45 @@
+﻿//페이지 로드 이벤트 잡기
+window.onload = Page_Load;
+
+function Page_Load()
+{
+    //이미지 검색
+    for (var i = 0; i < document.images.length; i++)
+    {
+        //부모가 A인 이미지만 필터링
+        if (document.images[i].parentNode.tagName == "A")
+        {
+            //롤오버 기능 구현
+            SetImgRollover(document.images[i]);
+        }
+
+    }
+}
+function SetImgRollover(img)
+{
+    //마우스 오버
+    img.MouseOverImg = new Image();  //이미지 개체 생성
+    img.MouseOverImg.src = "../Icon/" + img.id + "_over.jpg";
+    img.onmouseover = MouseOverhandler;
+
+    //마우스 아웃
+    img.MouseOutImg = new Image();  //이미지 개체 생성
+    img.MouseOutImg.src = img.src;
+    img.onmouseout = function () { this.src = this.MouseOutImg.src; }; //무명 함수(메서드)
+
+    //마우스 클릭
+    img.MouseClickImg = new Image();  //이미지 개체 생성
+    img.MouseClickImg.src = "../Icon/K-015.jpg";
+    img.onmousedown = MouseClickhandler;
+
+}
+
+function MouseOverhandler()
+{
+    this.src = this.MouseOverImg.src;
+}
+
+function MouseClickhandler()
+{
+    this.src = this.MouseClickImg.src;
+}
---
+++ JavaScript/JavaScript/Script/02/02_03_초간단슬라이드쇼.js
@@ -0,0 +1,27 @@
+﻿window.onload = function ()
+{
+    //슬라이드쇼에 사용할 이미지들
+    var myPhotos = new Array("../Icon/K-001.jpg", "../Icon/K-002.jpg", "../Icon/K-003.jpg");
+    var index = 0;
+
+    //이전 링크 클릭
+    document.getElementById("btnPrev").onclick = function ()
+    {
+        if (index == 0)
+        {
+            index = myPhotos.length;
+        }
+        document.getElementById("imgPlaceHolder").src = myPhotos[--index];
+        return false;
+    }
+
+    //다음 링크 클릭
+    document.getElementById("btnNext").onclick = function () {
+        if (++index == myPhotos.length)
+        {
+            index = 0;
+        }
+        document.getElementById("imgPlaceHolder").src = myPhotos[index];
+        return false;
+    }
+}
